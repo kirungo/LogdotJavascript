e593e9731dff1ca72bfe754f13d7eae227295724
kirungo <terrikirungo@gmail.com>
2026-09-15 15:06:34 +0300

    using the ternary operator to do the bmi comparison

--- 6-objects-in-functions.js
+++ 6-objects-in-functions.js
@@ -1,31 +1,40 @@
 const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  }
+	fullName: 'Mark Miller',
+	mass: 78,
+	height: 1.69,
+	calcBMI: function () {
+		this.bmi = this.mass / (this.height * this.height);
+		return this.bmi;
+	}
 };
 
 const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  }
+	fullName: 'John Smith',
+	mass: 92,
+	height: 1.95,
+	calcBMI: function () {
+		this.bmi = this.mass / (this.height * this.height);
+		return this.bmi;
+	}
 };
 
 if (mark.calcBMI() > john.calcBMI()) {
-  console.log(
-    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
-  );
+	console.log(
+		`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+	);
 } else {
-  console.log(
-    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
-  );
+	console.log(
+		`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+	);
 }
+
+
+/** Using the ternary operator format,
+ * this is how the code would look like
+ * condition ? valueIfTrue : valueIfFalse
+ */
+mark.calcBMI() > john.calcBMI()
+	? console.log(`${mark.fullName}'s BMI is higher!`)
+	: console.log(`${john.fullName}'s BMI is higher!`);
+
+
